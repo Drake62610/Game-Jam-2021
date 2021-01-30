d9b398958a0ad58ba5801855519d5f1b193e2478
Dralok <dralok75@gmail.com>
2021-01-30 02:44:27 +0100

    Smell Path + TestSceneSmell

---
+++ Assets/Scripts/Effects/ParticleFollowPath.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleFollowPath : MonoBehaviour
+{
+    public string pathName;
+    public float time;  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path",iTweenPath.GetPath(pathName), "easetype", iTween.EaseType.easeInOutSine, "time", time));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
