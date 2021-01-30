183bb035d131e8887087133420a912704c14e236
Drake62610 <drake@DESKTOP-LJ8P0SK.localdomain>
2021-01-30 13:04:33 +0100

    A lot of crazy stuff

--- Assets/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
+++ Assets/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
@@ -5,7 +5,7 @@ public class ScrollingUVs_Layers : MonoBehaviour
 {
 	//public int materialIndex = 0;
 	public Vector2 uvAnimationRate = new Vector2( 1.0f, 0.0f );
-	public string textureName = "_MainTex";
+	public string textureName = "lava1";
 	
 	Vector2 uvOffset = Vector2.zero;
 	
