0008e68e8a3706fae3d8c859f7fac35bfd073db9
Drake62610 <drake@DESKTOP-LJ8P0SK.localdomain>
2021-01-30 01:05:59 +0100

    Add Jump Bug Fix

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour
 {
     public float speed = 4f;
-    public float maxVelocityChange = 10.0f;
-    public float jumpForce = 250f;
+    public float jumpForce = 5f;
 
     public bool isGrounded;
     public bool isCrouched;
@@ -24,45 +23,16 @@ public class FPSInput : MonoBehaviour
     {       
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        
         _inputJump = Input.GetButtonDown("Jump");
 
-        Vector3 movement = new Vector3(moveHorizontal * speed, _rb.velocity.y, moveVertical * speed);
+        Vector3 movement = new Vector3(moveHorizontal * speed * Time.deltaTime, 0, moveVertical * speed * Time.deltaTime);
         movement = Vector3.ClampMagnitude(movement, speed);
-        _rb.velocity = transform.TransformDirection(movement);
-        // **
-        // Calculate how fast we should be moving
-		// Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		// targetVelocity = transform.TransformDirection(targetVelocity);
-		// targetVelocity *= speed;
- 
-		// // Apply a force that attempts to reach our target velocity
-		// Vector3 velocity = rb.velocity;
-		// Vector3 velocityChange = (targetVelocity - velocity);
-		// velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-		// velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-		// velocityChange.y = 0;
-		// rb.AddForce(velocityChange, ForceMode.VelocityChange);
-        // **
+        transform.Translate(movement.x, 0, movement.z);
 
-        // // Character Movement With Colisions
-        // float deltaX = Input.GetAxis("Horizontal") * speed;
-        // float deltaZ = Input.GetAxis("Vertical") * speed;
-        // Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-        // movement = Vector3.ClampMagnitude(movement, speed);
-        // // Gravity Force On PLayer
-        // movement *= Time.deltaTime;
-        // movement = transform.TransformDirection(movement);
-
-        //_charController.Move(movement);
         if(isGrounded && _inputJump){
-            _rb.AddForce(Vector3.up * jumpForce);
-            Debug.Log("jump");
+            _rb.AddForce(new Vector3( 0, jumpForce, 0), ForceMode.Impulse);
         }
-    }
-
-    void FixedUpdate() {
-        //Jump
-        
     }
 
     void OnCollisionEnter(Collision other) {
