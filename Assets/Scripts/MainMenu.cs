bda3b8d03cee100f8233c6e403630974a47cd5ab
yarimoto <alexisguys@gmail.com>
2021-01-31 12:28:14 +0100

    Build Settings

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,14 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
 
-    public string levelToLoad;
-
     public void StartGame()
     {
-        SceneManager.LoadScene(levelToLoad);
+        SceneManager.LoadScene(1);  
     }
 
     public void QuitGame()
