61501a6f0e47c7a5f128a99e02617cb6d941e23c
yarimoto <alexisguys@gmail.com>
2021-01-31 09:16:00 +0100

    Add smell scene

---
+++ Assets/Scripts/TriggerableObjects/EndStage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour, ITriggerableObject
+{
+
+    public List<GameObject> smellsPaths;
+    public GameObject orbPath;
+    public GameObject senseOrb;
+
+    public void Trigger()
+    {
+        foreach (var smellPath in smellsPaths)
+        {
+            smellPath.SetActive(false);
+        }
+        orbPath.SetActive(true);
+        senseOrb.SetActive(true);
+    }
+}
