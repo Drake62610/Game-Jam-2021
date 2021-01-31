b192c3c0d8a6db91f18b4526e5f057d26b7c18eb
Drake62610 <drake@DESKTOP-LJ8P0SK.localdomain>
2021-01-31 10:18:20 +0100

    Final Commit before huge mess

---
+++ Assets/Scripts/FootStepTrigger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepTrigger : MonoBehaviour
+{
+
+    private AudioSource _footsteps;
+    // Start is called before the first frame update
+    public bool IsMoving;
+    private void Start()
+    {
+        _footsteps = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetAxis("Vertical") < 0) IsMoving = true; 
+        else IsMoving = false;
+
+        if (IsMoving && !_footsteps.isPlaying) _footsteps.Play(); 
+        if (!IsMoving) _footsteps.Stop(); 
+    // void OnCollisionEnter(Collision other)
+    // {
+    //     if (other.gameObject.tag == "Player" && (Input.GetAxis("Vertical") < 0) {
+    //         if (!_footsteps.isPlaying)
+    //         {
+    //             _footsteps.Play(0);
+    //         }
+    //         else
+    //             _footsteps.Stop();
+    //     }
+    // }
+    }
+}
